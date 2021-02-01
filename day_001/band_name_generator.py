7d169292fc7b64f377b53afd965e7927c76ddc14
Raluca Drăghicescu <64273721+raiscreative@users.noreply.github.com>
2021-02-01 18:47:17 +0200

    Add files via upload

---
+++ day_001/band_name_generator.py
@@ -0,0 +1,4 @@
+print('Hello!\nWelcome to the Band Name Generator!\nPlease answer a couple of Pquestions to get an idea for your band name.')
+town = input('Firstly, tell us what is your hometown.\n')
+pet = input('What a great town! Now, what is the name of your pet?\n')
+print('Amazing!\nHow about naming your band ' + str(town) + ' ' + str(pet) + '?')
---
+++ day_001/switch_exercise.py
@@ -0,0 +1,10 @@
+a = input('Your first number: ')
+b = input('Your second number: ')
+print('**************************')
+print('      SWITCHING NOW       ')
+print('**************************')
+c = a
+a = b
+b = c
+print('a:', a)
+print('b:', b)
