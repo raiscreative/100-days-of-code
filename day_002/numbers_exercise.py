d0a3531525797b481dd98f0e5005d9bfd14bdaed
Raluca Drăghicescu <64273721+raiscreative@users.noreply.github.com>
2021-02-01 20:37:34 +0200

    Add files via upload

---
+++ day_002/numbers_exercise.py
@@ -0,0 +1,4 @@
+print("Hello! Let's play a little game!")
+the_number = input('Type a two digits number, please: ')
+result = int(the_number[0]) + int(the_number[1])
+print('The result is ' + str(result))
