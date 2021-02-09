b42b1ec136c1a8182d19957aaf91e918c1551726
Raluca Drăghicescu <64273721+raiscreative@users.noreply.github.com>
2021-02-09 14:39:38 +0200

    Delete day_015/day_016 directory

--- day_015/day_016/table.py
+++
@@ -1,8 +0,0 @@
-from prettytable import PrettyTable
-
-table = PrettyTable()
-
-table.add_column('Student', ['Harry', 'Ron', 'Hermione', 'Neville', 'Draco'])
-table.add_column('grade', ['Acceptable', 'Acceptable', 'Outstanding', 'Exeeds expectations', 'Troll'])
-
-print(table)
