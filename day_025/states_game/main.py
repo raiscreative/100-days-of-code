ff688cb0cd49cad1b094d123820e030836a1dc9c
Raluca Drăghicescu <64273721+raiscreative@users.noreply.github.com>
2021-02-18 16:31:35 +0200

    Update main.py

--- day_025/states_game/main.py
+++ day_025/states_game/main.py
@@ -10,15 +10,13 @@ turtle.shape(image)
 
 data = pd.read_csv('50_states.csv')
 states = data.state.to_list()
-states_set = set(states)
 guessed_states = []
 
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
     if answer_state == 'Exit':
-        guessed_set = set(guessed_states)
-        missing_states = list(states_set.difference(guessed_set))
+        missing_states = [state for state in states if state not in guessed_states]
         new_data = pd.DataFrame(missing_states)
         new_data.to_csv('states_to_learn.csv')
         break
