3b9fc50fcb62ae35418abd2db04905a4011b1495
Raluca Drăghicescu <64273721+raiscreative@users.noreply.github.com>
2021-02-18 16:49:04 +0200

    Add files via upload

--- day_025/states_game/main.py
+++ day_025/states_game/main.py
@@ -13,8 +13,8 @@ states = data.state.to_list()
 guessed_states = []
 
 while len(guessed_states) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
-                                    prompt="What's another state's name?").title()
+    answer_state = screen.textinput(title=f'{len(guessed_states)}/50 States Correct',
+                                    prompt='What\'s another state\'s name?').title()
     if answer_state == 'Exit':
         missing_states = [state for state in states if state not in guessed_states]
         new_data = pd.DataFrame(missing_states)
