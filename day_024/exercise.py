1f83bcf44b4d59111c1a6db0092f076ad9442f41
Raluca Drăghicescu <64273721+raiscreative@users.noreply.github.com>
2021-02-22 00:01:56 +0200

    Update exercise.py

--- day_024/exercise.py
+++ day_024/exercise.py
@@ -5,3 +5,11 @@ def statistics(file_name):
     return { "lines": len(lines),
              "words": sum(len(line.split(" ")) for line in lines),
              "characters": sum(len(line) for line in lines) }
+
+def find_and_replace(file_name, old_word, new_word):
+    with open(file_name, "r+") as file:
+        text = file.read()
+        new_text = text.replace(old_word, new_word)
+        file.seek(0)
+        file.write(new_text)
+        file.truncate()
