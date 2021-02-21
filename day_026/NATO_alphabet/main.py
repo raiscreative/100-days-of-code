7d75631698638d45a4ac76401244f49e6f27b0bf
Raluca Drăghicescu <64273721+raiscreative@users.noreply.github.com>
2021-02-21 18:31:48 +0200

    Add files via upload

--- day_026/NATO_alphabet/main.py
+++ day_026/NATO_alphabet/main.py
@@ -2,11 +2,17 @@ import pandas as pd
 
 data = pd.read_csv('nato_phonetic_alphabet.csv')
 
-
-
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(nato_dict)
 
-message = input('What do you have to say? Speak clearly! ').upper()
-phonetic_message = [nato_dict[char] for char in message]
-print(phonetic_message)
+
+def generate_phonetic_message():
+    message = input('What do you have to say? Speak clearly! ').upper()
+    try:
+        phonetic_message = [nato_dict[char] for char in message]
+    except KeyError:
+        print('Please use only characters from the alphabet.')
+        generate_phonetic_message()
+    else:
+        print(phonetic_message)
+
+generate_phonetic_message()
